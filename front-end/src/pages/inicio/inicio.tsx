fa7f3831e0c3df15731a276ff5c2469dc76e5cc6
Joao Henrique Damazio <jhenriqd@everis.com>
2019-05-16 11:14:06 -0300

    adicionado typescript e scss

---
+++ front-end/src/pages/inicio/inicio.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './inicio.scss';
+
+function Inicio() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Tela de inicio
+                </p>
+           </header>
+        </div>
+    );
+}
+
+export default Inicio;
