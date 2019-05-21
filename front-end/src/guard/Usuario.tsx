a008f48ae4547bef9381157ae8274daaadf077ff
Joao Henrique Damazio <jhenriqd@everis.com>
2019-05-21 09:31:05 -0300

    Adicionado local storage

--- front-end/src/guard/Usuario.tsx
+++ front-end/src/guard/Usuario.tsx
@@ -3,11 +3,6 @@ import {LocalStorage} from "../helpers/LocalStorage";
 export class Usuario {
 
   nome: string = "";
-  jaComecouJogo: boolean = false;
-  tema: string = "";
-  saiuJogo: number = 0;
-  vidas: number = 3;
-  pontuacao: number = 0;
 
   static entrar(nome: string) {
 
@@ -31,37 +26,6 @@ export class Usuario {
 
   }
 
-  static iniciarJogo(tema: string) {
 
-    let usuario = LocalStorage.pegarDados("usuario");
-
-    usuario.jaComecouJogo = true;
-    usuario.tema = tema;
-    usuario.saiuJogo = 0;
-    usuario.vidas = 3;
-
-    LocalStorage.salvarDados("usuario", usuario);
-
-  }
-
-  static acertar() {
-
-    let usuario = LocalStorage.pegarDados("usuario");
-
-    usuario.pontuacao++;
-
-    LocalStorage.salvarDados("usuario", usuario);
-
-  }
-
-  static errar() {
-
-    let usuario = LocalStorage.pegarDados("usuario");
-
-    usuario.vidas--;
-
-    LocalStorage.salvarDados("usuario", usuario);
-
-  }
 
 }
