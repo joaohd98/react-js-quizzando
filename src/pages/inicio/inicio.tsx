03d76a537f91978f635fc155f77219a31c630d9d
Joao Henrique Damazio <jhenriqd@everis.com>
2019-06-01 14:55:09 -0300

    Arrumado build

--- src/pages/inicio/inicio.tsx
+++ src/pages/inicio/inicio.tsx
@@ -12,9 +12,7 @@ class Inicio extends React.Component {
 
   constructor(props: any){
     super(props);
-
-    alert(process.env.REACT_APP_SECRET_CODE);
-
+    
     let nome: StateInterface = {
       validations: [
         {
